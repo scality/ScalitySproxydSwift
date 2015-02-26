217610cbe607854938738cca1e8b6086d62e8702
Nicolas Trangez <ikke@nicolast.be>
2015-02-26 12:38:41 +0100

    CI: Tox failures are no errors
    (cherry picked from commit 5616fe3eb758d8a8459fface05688b33f73835da)
    Signed-off-by: Nicolas Trangez <ikke@nicolast.be>

--- jenkins/swift-tox/20-run-tox.sh
+++ jenkins/swift-tox/20-run-tox.sh
@@ -7,14 +7,35 @@ rm -f $XUNIT_FILE
 export NOSE_WITH_XUNIT=1
 export NOSE_XUNIT_FILE=$XUNIT_FILE
 
-# We need at least one test, otherwise Jenkins' JUnit reporter gets cranky
-if test "x${TOXENV}" == "xpep8"; then
-        cat > $XUNIT_FILE << EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<testsuite name="nosetests" tests="1" errors="0" failures="0" skip="0">
-    <testcase classname="test" name="test_noop" time="0.000" />
-</testsuite>
-EOF
+set +e
+tox
+TOXRC=$?
+set -e
+
+if test $TOXRC -ne 0; then
+        echo "tox run unsuccessful, build unstable"
 fi
 
-tox
+
+# We need at least one test, otherwise Jenkins' JUnit reporter gets cranky
+if test "x${TOXENV}" == "xpep8"; then
+        if test $TOXRC -eq 0; then
+                cat > $XUNIT_FILE << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<testsuite name="tox-pep8" tests="1" errors="0" failures="0" skip="0">
+    <testcase classname="pep8" name="run" time="0.000" />
+</testsuite>
+EOF
+        else
+                cat > $XUNIT_FILE << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<testsuite name="tox-pep8" tests="1" errors="0" failures="1" skip="0">
+    <testcase classname="pep8" name="run" time="0.000">
+        <failure type="exceptions.AssertionError" message="PEP8 failed">
+            See console log
+        </failure>
+    </testcase>
+</testsuite>
+EOF
+        fi
+fi
