d270f2eabdc92e38b00eb76be9ecd6faa90d94b7
Nicolas Trangez <ikke@nicolast.be>
2015-03-05 15:27:15 +0100

    Basic unit-tests for storage policy support in `ObjectServer`

--- test/unit/test_server.py
+++ test/unit/test_server.py
@@ -15,12 +15,22 @@
 
 '''Tests for `swift_scality_backend.server`'''
 
+import errno
 import inspect
+import os
+import StringIO
+import unittest
+import urlparse
+
+import mock
 
 import swift.obj.server
 
 import swift_scality_backend.diskfile
+import swift_scality_backend.policy_configuration
 import swift_scality_backend.server
+
+from . import utils
 
 
 def test_api_compatible():
@@ -89,3 +99,102 @@ def test_get_diskfile():
     assert diskfile._account == 'a'
     assert diskfile._container == 'c'
     assert diskfile._obj == 'o'
+
+
+_mock_policy_config_file = mock.mock_open()
+_mock_policy_config_file.side_effect = IOError(errno.ENOENT, 'Mock')
+
+
+class FakeFile(StringIO.StringIO):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *_):
+        pass
+
+
+@mock.patch('eventlet.spawn', mock.Mock())
+@mock.patch('__builtin__.open', _mock_policy_config_file)
+class TestStoragePolicySupport(unittest.TestCase):
+    def test_no_policy(self):
+        with mock.patch(
+                'swift_scality_backend.server.ObjectController._get_client_for_policy') as mock_gc:
+            serv = swift_scality_backend.server.app_factory({})
+
+            serv.get_diskfile('dev', 'partition', 'a', 'c', 'o')
+
+            mock_gc.assert_called_with(0)
+
+    def test_policy_0(self):
+        with mock.patch(
+                'swift_scality_backend.server.ObjectController._get_client_for_policy') as mock_gc:
+            serv = swift_scality_backend.server.app_factory({})
+
+            serv.get_diskfile('dev', 'partition', 'a', 'c', 'o', policy_idx=0)
+
+            mock_gc.assert_called_with(0)
+
+    def test_policy_1(self):
+        with mock.patch(
+                'swift_scality_backend.server.ObjectController._get_client_for_policy') as mock_gc:
+            serv = swift_scality_backend.server.app_factory({})
+
+            serv.get_diskfile('dev', 'partition', 'a', 'c', 'o', policy_idx=1)
+
+            mock_gc.assert_called_with(1)
+
+    def test_policy_file_read_error(self):
+        mock_open = mock.mock_open()
+        mock_open.side_effect = IOError(errno.ENFILE, os.strerror(errno.ENFILE))
+
+        with mock.patch('__builtin__.open', mock_open):
+            self.assertRaises(
+                IOError,
+                swift_scality_backend.server.app_factory, {})
+
+    def test_policy_request_without_configuration(self):
+        serv = swift_scality_backend.server.app_factory({})
+
+        self.assertRaises(
+            RuntimeError,
+            serv.get_diskfile, 'dev', 'partition', 'a', 'c', 'o', policy_idx=1)
+
+    def test_broken_policy_configuration(self):
+        policy = '\n'.join(line.strip() for line in '''
+            [storage-policy:1]
+            write = no-such-ring
+            '''.splitlines())
+
+        mock_open = mock.mock_open()
+        mock_open.return_value = FakeFile(policy)
+
+        with mock.patch('__builtin__.open', mock_open):
+            utils.assertRaisesRegexp(
+                swift_scality_backend.policy_configuration.ConfigurationError,
+                'Unknown \'write\' ring \'no-such-ring\' in policy 1',
+                swift_scality_backend.server.app_factory, {})
+
+    def test_simple_policy_configuration(self):
+        policy = '\n'.join(line.strip() for line in '''
+            [ring:paris]
+            location = paris
+            sproxyd_endpoints = http://localhost:8080/chord, http://otherhost:8080/chord
+
+            [storage-policy:1]
+            read =
+            write = paris
+            '''.splitlines())
+
+        mock_open = mock.mock_open()
+        mock_open.return_value = FakeFile(policy)
+
+        with mock.patch('__builtin__.open', mock_open):
+            serv = swift_scality_backend.server.app_factory({})
+
+            df = serv.get_diskfile(
+                'dev', 'partition', 'a', 'c', 'o', policy_idx=1)
+
+            self.assertEqual(frozenset([
+                urlparse.urlparse('http://localhost:8080/chord'),
+                urlparse.urlparse('http://otherhost:8080/chord'),
+            ]), df._filesystem._alive)
