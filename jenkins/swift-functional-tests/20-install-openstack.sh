4f9f74428aa58cb5f5910587192d51eb96ade033
Emmanuel Cazenave <contact@emcaz.fr>
2015-03-25 14:24:54 +0100

    Icehouse and master support require some tweaking.

--- jenkins/swift-functional-tests/20-install-openstack.sh
+++ jenkins/swift-functional-tests/20-install-openstack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -xue
 
+
 function common {
     git clone -b ${DEVSTACK_BRANCH} https://github.com/openstack-dev/devstack.git
     cp devstack/samples/local.conf devstack/local.conf
@@ -9,20 +10,37 @@ enable_service key mysql s-proxy s-object s-container s-account
 SCREEN_LOGDIR="\${DEST}/logs"
 EOF
     cp jenkins/${JOB_NAME%%/*}/extras.d/55-swift-sproxyd.sh devstack/extras.d/55-swift-sproxyd.sh
+    if [[ $DEVSTACK_BRANCH == "stable/icehouse" ]]; then
+        # Workaound depencies version conflict :
+        # last python-glanceclient (0.17 at the time of writing), wich gets installed by default,
+        # requires keystoneclient >= 1.0.0
+        # whereas keystone requires keystoneclient <= 0.11.2
+        # python-glanceclient >= 0.13.1 is required by openstackclient 0.4.1
+        sudo pip install python-glanceclient==0.13.1
+    fi
     ./devstack/stack.sh
+}
+
+function ubuntu_common {
+    wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python;
+    sudo aptitude install -y python-pip
+    if [[ $DEVSTACK_BRANCH == 'master' ]]; then
+        sudo aptitude install -y make
+        sudo aptitude install -y xfsprogs
+    elif [[ $DEVSTACK_BRANCH == "stable/icehouse" ]]; then
+        sudo aptitude install -y gcc python-dev
+    fi
 }
 
 function ubuntu14_specifics {
     # Workaround pip upgrading six without completely removing the old one
     # which then cause an error.
-    wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python;
     sudo easy_install -U six
 }
 
 function ubuntu12_specifics {
     # Workaround pip upgrading cmd2 without completely removing the old one
     # which then cause an error.
-    wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python;
     sudo easy_install -U cmd2
 }
 
@@ -30,11 +48,21 @@ function centos_specifics {
     sudo yum install -y wget
     wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python;
     sudo easy_install -U six
+    sudo yum install -y python-pip
+    if [[ $DEVSTACK_BRANCH == "stable/icehouse" ]]; then
+        # Required to get 'cryptography' python package compiled during its installation through pip.
+        sudo yum install -y gcc python-devel libffi-devel openssl-devel
+        # Required by keystoneclient
+        sudo yum install -y MySQL-python
+        # devstack uses the ip command.
+        export PATH=$PATH:/sbin/
+    fi
 }
 
 function main {
     source jenkins/openstack-ci-scripts/jenkins/distro-utils.sh
     if is_ubuntu; then
+        ubuntu_common
         if [[ $os_CODENAME == "precise" ]]; then
             ubuntu12_specifics
         elif [[ $os_CODENAME == "trusty" ]]; then
