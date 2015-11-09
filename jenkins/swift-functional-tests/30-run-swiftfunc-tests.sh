0b29a4bd81ca66181782d8e63067e358871b109b
Jordan Pittier <jordan.pittier@scality.com>
2015-11-09 12:31:15 +0100

    Rework the Swift functional job
    Main changes:
      * Launch tests in a venv using Tox
      * Only install required OpenStack services (i.e only Swift)

--- jenkins/swift-functional-tests/30-run-swiftfunc-tests.sh
+++ jenkins/swift-functional-tests/30-run-swiftfunc-tests.sh
@@ -1,11 +1,7 @@
 #!/bin/bash -xue
-source jenkins/openstack-ci-scripts/jenkins/distro-utils.sh
-if is_centos; then
-    sudo pip install -U nose
-fi
-SWIFT_DIR=/opt/stack/swift
-sudo pip install -r $SWIFT_DIR/test-requirements.txt
-set +e
-nosetests -v -w $SWIFT_DIR/test/functional --exe --with-xunit --xunit-file=${WORKSPACE}/swift-func-tests.xml
-set -e
 
+cd /opt/stack/swift
+
+set +e
+tox -e func -- test/functional --with-xunit --xunit-file=${WORKSPACE}/swift-func-tests.xml
+set -e
--- jenkins/swift-functional-tests/50-collect-artifacts.sh
+++ jenkins/swift-functional-tests/50-collect-artifacts.sh
@@ -2,14 +2,17 @@
 
 echo "Entering WORKSPACE."
 cd $WORKSPACE
-mkdir jenkins-logs
+
 echo "Creating jenkins-log directory."
+mkdir jenkins-logs
 cp -R /opt/stack/logs/* jenkins-logs/
+
 if [[ -f "/var/log/messages" ]]; then
     sudo cp /var/log/messages jenkins-logs/messages
 fi
+
 if [[ -f "/var/log/syslog" ]]; then
     sudo cp /var/log/syslog jenkins-logs/syslog
 fi
+
 sudo chown jenkins jenkins-logs/*
-exit 0;
