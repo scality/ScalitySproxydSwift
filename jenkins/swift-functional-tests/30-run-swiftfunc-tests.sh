ced87cd47d525ebd070f571989fcb176f447f655
Jordan Pittier <jordan.pittier@scality.com>
2015-12-18 11:20:13 +0100

    Use subunit2junitxml to generate the XML report
    Swift doesn't use `nosetests` anymore but `testr`. See
    https://review.openstack.org/#/c/214206/

--- jenkins/swift-functional-tests/30-run-swiftfunc-tests.sh
+++ jenkins/swift-functional-tests/30-run-swiftfunc-tests.sh
@@ -3,5 +3,13 @@
 cd /opt/stack/swift
 
 set +e
-tox -e func -- test/functional --with-xunit --xunit-file=${WORKSPACE}/swift-func-tests.xml
+if [[ ${DEVSTACK_BRANCH} == "stable/juno" ]] || [[ ${DEVSTACK_BRANCH} == "stable/kilo" ]] || [[ ${DEVSTACK_BRANCH} == "stable/liberty" ]]; then
+    tox -e func -- test/functional --with-xunit --xunit-file=${WORKSPACE}/swift-func-tests.xml
+else
+    tox -e func
+fi
+
+if [[ ${DEVSTACK_BRANCH} == "stable/mitaka" ]] || [[ ${DEVSTACK_BRANCH} == "master" ]]; then
+    testr last --subunit | subunit2junitxml -o ${WORKSPACE}/swift-func-tests.xml
+fi
 set -e
