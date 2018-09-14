4e5c7dccafaec11aef1b0df17b603c113f1025f4
jgagnaire <jean.gagnaire@scality.com>
2018-09-14 16:06:08 +0200

    Release: Fix version to '0.4.2'

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ def get_version():
 
 setuptools.setup(
     name='swift-scality-backend',
-    version=get_version(),
+    version='0.4.2',
     description='Scality Ring backend for OpenStack Swift',
     url='http://www.scality.com/',
     author='Scality Openstack Engineering Team',
